a51e4b808ef537c4b44051aeceb3c586945357f2
Ali Jan <alijandeveloper15@gmail.com>
2025-03-15 23:45:16 -0700

    server satup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
-require("dotenv").config();
+
+dotenv.config();
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Sample Route
+
+
 app.get("/", (req, res) => {
-    res.send("🚀 Server is Running...");
+  res.send("Portfolio Backend Running...");
 });
 
-// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
