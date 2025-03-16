b69fe76a0a676789fca35dc64118d6eb8bfb38b3
Ali Jan <alijandeveloper15@gmail.com>
2025-03-16 12:49:22 -0700

    auth Middleware add

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addProject, getProjects, updateProject, deleteProject } from "../controllers/projectController.js";
+import authMiddleware from "../middleware/authMiddleware.js"; // 🛡️ Import middleware
 
 const router = express.Router();
 
-router.post("/add", addProject);
+router.post("/add", authMiddleware, addProject);
 router.get("/", getProjects);
-router.put("/:id", updateProject);
-router.delete("/:id", deleteProject);
+router.put("/:id", authMiddleware, updateProject);
+router.delete("/:id", authMiddleware, deleteProject);
 
 export default router;
